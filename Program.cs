95f435873055d5f3be292f9adba9a94bfe900019
scrviy <91909966+scrviy@users.noreply.github.com>
2023-01-03 16:36:53 -0300

    alguns exercicios mslearn

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Exercicios
+{
+    public class Exercicio
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("a" == "a");
+             Console.WriteLine("a" == "A");
+             Console.WriteLine(1 == 2);
+
+             string myValue = "a";
+             Console.WriteLine(myValue == "a");
+
+             string value1 = " a";
+             string value2 = "A ";
+             Console.WriteLine(value1.Trim().ToLower() == value2.Trim().ToLower());
+
+             Console.WriteLine("a" != "a");
+             Console.WriteLine("a" != "A");
+             Console.WriteLine(1 != 2);
+
+             string myValue1 = "a";
+             Console.WriteLine(myValue1 != "a");
+
+             Console.WriteLine(1 > 2);
+             Console.WriteLine(1 < 2);
+             Console.WriteLine(1 >= 1);
+             Console.WriteLine(1 <= 1);
+
+             string pangram = "The quick brown fox jumps over the lazy dog.";
+             Console.WriteLine(pangram.Contains("fox"));
+             Console.WriteLine(pangram.Contains("cow"));
+
+            // //* These two lines of code do the same thing
+             Console.WriteLine(pangram.Contains("fox") == false);
+             Console.WriteLine(!pangram.Contains("fox"));
+
+            string pangram1 = "The quick brown fox jumps over the lazy dog.";
+            Console.WriteLine(!pangram1.Contains("fox"));
+            Console.WriteLine(!pangram1.Contains("cow"));
+        }
+    }
+}
